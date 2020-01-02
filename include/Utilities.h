fffb87d47b2a900c17424f2e1a4d4eb35c208efb
Daniel Dreher <danieldreher@protonmail.com>
2020-01-02 16:29:00 +0100

    Add utility function definition in header

--- include/Utilities.h
+++ include/Utilities.h
@@ -8,4 +8,5 @@ class Utility
 {
 public:
     static std::pair<ReturnCode, std::string> ReadFile(const std::string& file_name);
+    static uint32_t GetNextMultipleOf(uint32_t num_to_round, uint32_t num_multiple);
 };
